797aca9a815fd10d0d52491582c68dabd51d8cd8
Omer Mordechai <81302085+OMER62@users.noreply.github.com>
2022-11-20 09:33:29 +0200

    Add files via upload
    20.11

--- test/TestDFS.java
+++ test/TestDFS.java
@@ -8,20 +8,27 @@ import algorithms.search.*;
 import java.util.ArrayList;
 public class TestDFS {
     public static void main(String[] args) throws Exception {
-        /**** Basic test Unit 1: */
+        //Step A: Generate+ print the maze:
         AMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(999, 999);
-        SearchableMaze searchableMaze = new SearchableMaze(maze);
-        ISearchingAlgorithm domain = new BestFirstSearch();
+        Maze maze = mg.generate(10, 10);
+        System.out.println("The maze:");
         maze.print();
+
+        //Step B: adaptations the search algorithm for Maze object
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
+
+        //Step C: Solved the Maze:
+        ISearchingAlgorithm domain = new BestFirstSearch();
         long StartTime=System.currentTimeMillis();
         //we operate the generate method
         Solution solution = domain.solve(searchableMaze);
         //we save the end time of the method
         long EndTime=System.currentTimeMillis();
+        System.out.println("The Path:");
         System.out.println(solution.getSolutionPath().toString());
+        System.out.println("The Total vertex in the path:");
         System.out.println(solution.getTotalVertx());
-
+        System.out.println("The Total time (in milliseconds):");
         System.out.println(EndTime-StartTime);
 
     }
